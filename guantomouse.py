d10db92f87368791bfa9d9f5f0e2ae1dc351c788
Niktronics <124365519+Niktronics@users.noreply.github.com>
2025-12-21 10:21:34 +0100

    Refactor to use pydirectinput for mouse control

--- guantomouse.py
+++ guantomouse.py
@@ -1,36 +1,3 @@
-# import asyncio
-# import websockets
-# import vgamepad as vg
-
-# gamepad = vg.VX360Gamepad()
-
-# async def handler():
-#     uri = "ws://192.168.1.22"
-#     async with websockets.connect(uri) as websocket:
-#         async for message in websocket:
-#             try:
-#                 if message == "LP":
-#                     gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
-#                 elif message == "LR":
-#                     gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER)
-#                 elif message == "RP":
-#                     gamepad.press_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
-#                 elif message == "RR":
-#                     gamepad.release_button(vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER)
-#                 else:
-#                     mx, my = map(int, message.split(","))
-#                     gx = mx * 32767 // 10
-#                     gy = my * 32767 // 10
-#                     gamepad.left_joystick(x_value=gx, y_value=-gy)
-#                     print("Joystick:", gx, gy)
-
-#                 gamepad.update()
-#             except Exception as e:
-#                 print("Errore parsing:", e)
-
-# asyncio.run(handler())
-
-
 import asyncio
 import websockets
 import pydirectinput
@@ -58,3 +25,4 @@ async def handler():
                 print("Errore parsing:", e)
 
 asyncio.run(handler())
+
